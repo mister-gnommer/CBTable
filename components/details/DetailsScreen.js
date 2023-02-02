ea052833b931e5223c3f49c3b12a4185d4afb064
mister-gnommer <kris@gnommer.pl>
2023-02-02 23:06:10 +0100

    Handle removing thought
    #6

--- components/details/DetailsScreen.js
+++ components/details/DetailsScreen.js
@@ -1,10 +1,32 @@
 import React from "react"
-import { Button, StyleSheet, Text, View } from "react-native"
+import { Alert, Button, StyleSheet, Text, View } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 import { globalStyles } from "../../globalStyles"
 import { stepsDescriptions } from "../../stepsDescriptions"
 
 const DetailsScreen = ({ thought, setScreen }) => {
+  const handleDeletingThought = async () => {
+    try {
+      await AsyncStorage.removeItem(`thought-${thought.timestamp}`)
+      setScreen("home")
+    } catch (err) {
+      Alert.alert("Błąd", "Nie udało się usunąć wpisu.")
+    }
+  }
+
+  const showDeleteAlert = () => {
+    Alert.alert("Usuń wpis", "Czy na pewno chcesz usunąć ten wpis?", [
+      {
+        text: "Anuluj",
+      },
+      {
+        text: "Usuń",
+        onPress: handleDeletingThought,
+      },
+    ])
+  }
+
   return (
     <>
       <View style={styles.headerContainer}>
@@ -15,6 +37,8 @@ const DetailsScreen = ({ thought, setScreen }) => {
       </View>
       <View style={globalStyles.footerContainer}>
         <Button title="Wróć" onPress={() => setScreen("home")} />
+        <Button title="Usuń" onPress={showDeleteAlert} />
+        <Button title="Edytuj" onPress={() => setScreen("home")} />
       </View>
     </>
   )
